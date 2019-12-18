4bb4a94973ce865797c5211f4d3eccfc60db55eb
vikas sharma <vikas@lintelindia.com>
2019-12-18 17:35:24 +0530

    first commit

---
+++ PodTest/PodTest/PodTest.h
@@ -0,0 +1,19 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by Harsh Vardhan on 18/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTest.
+FOUNDATION_EXPORT double PodTestVersionNumber;
+
+//! Project version string for PodTest.
+FOUNDATION_EXPORT const unsigned char PodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTest/PublicHeader.h>
+
+
---
+++ PodTest/PodTest/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  PodTest
+//
+//  Created by Harsh Vardhan on 18/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+import Foundation
+public class Service{
+    
+    private init(){}
+
+    public static func doSomething() -> String {
+        return "This is test pod"
+    }
+}
